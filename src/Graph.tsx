719432176ef81caf85d0d67542ce243fb0deaefe
Preeti Arumalla <parumalla385@gmail.com>
2020-08-25 11:17:19 -0400

    Create Patch File

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,7 +32,22 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    // Continuous line
+    elem.setAttribute('view', 'y_line');
+    // Distinguish stock
+    elem.setAttribute('column-pivots', '["stock"]');
+    // X-axis
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    // Focus on top_ask_price
+    elem.setAttribute('columns', '["top_ask_price"]');
+    // Average out the top_bid_prices and the top_ask_prices for similar datapoints
+    elem.setAttribute('aggregates', `
+      {"stock": "distinct count",
+      "top_ask_price": "avg",
+      "top_bid_price": "avg",
+      "timestamp": "distinct count"}`);
 
     const schema = {
       stock: 'string',
